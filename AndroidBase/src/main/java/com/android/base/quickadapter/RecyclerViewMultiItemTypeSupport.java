471876ac100c34dd5abd81bc9314846818cd6883
huangwm <270621849@qq.com>
2015-11-04 21:53:19 +0800

    提交recyclerview快速构建adapter

--- AndroidBase/src/main/java/com/android/base/quickadapter/RecyclerViewMultiItemTypeSupport.java
+++
@@ -1,9 +0,0 @@
-package com.android.base.quickadapter;
-
-public interface RecyclerViewMultiItemTypeSupport<T> {
-
-    int getLayoutId(int viewType);
-
-    int getItemViewType(int position, T t);
-
-}
