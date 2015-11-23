c7e2211004dab059f62dd678ab1d0c2e45464a1c
huangwm <270621849@qq.com>
2015-11-23 23:28:43 +0800

    加入了鸿洋大神的终极分辨率适配方案AutoLayout

---
+++ AndroidWidget/src/main/java/com/android/base/widget/autolayout/AutoLinearLayout.java
@@ -0,0 +1,82 @@
+package com.android.base.widget.autolayout;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+/**
+ * Created by zhy on 15/6/30.
+ */
+public class AutoLinearLayout extends LinearLayout
+{
+
+    private AutoLayoutHelper mPercentLayoutHelper;
+
+    public AutoLinearLayout(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+        mPercentLayoutHelper = new AutoLayoutHelper(this);
+    }
+
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
+    {
+        if (!isInEditMode())
+            mPercentLayoutHelper.adjustChildren();
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b)
+    {
+        super.onLayout(changed, l, t, r, b);
+    }
+
+
+    @Override
+    public LayoutParams generateLayoutParams(AttributeSet attrs)
+    {
+        return new LayoutParams(getContext(), attrs);
+    }
+
+
+    public static class LayoutParams extends LinearLayout.LayoutParams
+            implements AutoLayoutHelper.AutoLayoutParams
+    {
+        private AutoLayoutHelper.AutoLayoutInfo mAutoLayoutInfo;
+
+        public LayoutParams(Context c, AttributeSet attrs)
+        {
+            super(c, attrs);
+            mAutoLayoutInfo = AutoLayoutHelper.getAutoLayoutInfo(c, attrs);
+        }
+
+        @Override
+        public AutoLayoutHelper.AutoLayoutInfo getPercentLayoutInfo()
+        {
+            return mAutoLayoutInfo;
+        }
+
+
+        public LayoutParams(int width, int height)
+        {
+            super(width, height);
+        }
+
+
+        public LayoutParams(ViewGroup.LayoutParams source)
+        {
+            super(source);
+        }
+
+        public LayoutParams(MarginLayoutParams source)
+        {
+            super(source);
+        }
+
+    }
+
+}
